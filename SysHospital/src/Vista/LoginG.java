165cbeebf27e42ee244436017c7211d907b1c855
EuliseMatias <eulise27matias@gmail.com>
2022-04-20 23:23:45 -0600

    Login
    Mejora del login, cambio de portada, evento enter agregado y boton exit

--- SysHospital/src/Vista/LoginG.java
+++ SysHospital/src/Vista/LoginG.java
@@ -3,6 +3,7 @@ package Vista;
 
 import Modelo.LoginDAO;
 import Modelo.Login;
+import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
 
 
@@ -55,145 +56,138 @@ public class LoginG extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        PanelLogin = new javax.swing.JPanel();
-        LabelIniciarIMG = new javax.swing.JLabel();
-        LabelTitulo = new javax.swing.JLabel();
+        IconoLogin = new javax.swing.JLabel();
         LabelUsuario = new javax.swing.JLabel();
-        LabelClave = new javax.swing.JLabel();
         TxtUsuario = new javax.swing.JTextField();
+        LabelClave = new javax.swing.JLabel();
         TXtClave = new javax.swing.JPasswordField();
         BtnIniciarLog = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        PanelPrincipal = new javax.swing.JPanel();
-        LabelFondoIMG = new javax.swing.JLabel();
+        BotonSalir = new javax.swing.JButton();
+        Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        PanelLogin.setBackground(new java.awt.Color(255, 255, 255));
+        IconoLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/IconoLogin.png"))); // NOI18N
+        getContentPane().add(IconoLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 30, -1, -1));
 
-        LabelIniciarIMG.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/iniciar.png"))); // NOI18N
+        LabelUsuario.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        LabelUsuario.setForeground(new java.awt.Color(102, 102, 102));
+        LabelUsuario.setText("User:");
+        getContentPane().add(LabelUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 240, 50, 20));
 
-        LabelTitulo.setBackground(new java.awt.Color(0, 51, 153));
-        LabelTitulo.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        LabelTitulo.setForeground(new java.awt.Color(0, 51, 204));
-        LabelTitulo.setText("B I E N V E N I D O");
-
-        LabelUsuario.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        LabelUsuario.setForeground(new java.awt.Color(0, 51, 204));
-        LabelUsuario.setText("USUARIO:");
-
-        LabelClave.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        LabelClave.setForeground(new java.awt.Color(0, 51, 204));
-        LabelClave.setText("CLAVE:");
-
+        TxtUsuario.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        TxtUsuario.setForeground(new java.awt.Color(51, 51, 51));
         TxtUsuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 TxtUsuarioActionPerformed(evt);
             }
         });
+        TxtUsuario.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                TxtUsuarioKeyPressed(evt);
+            }
+        });
+        getContentPane().add(TxtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 230, 230, 40));
 
-        BtnIniciarLog.setBackground(new java.awt.Color(0, 51, 204));
-        BtnIniciarLog.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
-        BtnIniciarLog.setForeground(new java.awt.Color(255, 255, 255));
-        BtnIniciarLog.setText("I N I C I A R");
+        LabelClave.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        LabelClave.setForeground(new java.awt.Color(102, 102, 102));
+        LabelClave.setText("Password:");
+        getContentPane().add(LabelClave, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 310, 100, 30));
+
+        TXtClave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TXtClaveActionPerformed(evt);
+            }
+        });
+        TXtClave.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                TXtClaveKeyPressed(evt);
+            }
+        });
+        getContentPane().add(TXtClave, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 300, 230, 40));
+
+        BtnIniciarLog.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        BtnIniciarLog.setForeground(new java.awt.Color(51, 51, 51));
+        BtnIniciarLog.setText("Sign in");
         BtnIniciarLog.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BtnIniciarLogActionPerformed(evt);
             }
         });
+        BtnIniciarLog.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                BtnIniciarLogKeyPressed(evt);
+            }
+        });
+        getContentPane().add(BtnIniciarLog, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 390, 120, 40));
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 3, 10)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("Randy Dev, todos los derechos reservados 2021");
+        BotonSalir.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        BotonSalir.setForeground(new java.awt.Color(255, 51, 0));
+        BotonSalir.setText("Exit");
+        BotonSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonSalirActionPerformed(evt);
+            }
+        });
+        BotonSalir.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                BotonSalirKeyPressed(evt);
+            }
+        });
+        getContentPane().add(BotonSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 450, 120, 40));
 
-        javax.swing.GroupLayout PanelLoginLayout = new javax.swing.GroupLayout(PanelLogin);
-        PanelLogin.setLayout(PanelLoginLayout);
-        PanelLoginLayout.setHorizontalGroup(
-            PanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PanelLoginLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(LabelTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29))
-            .addGroup(PanelLoginLayout.createSequentialGroup()
-                .addGroup(PanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(PanelLoginLayout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(PanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(PanelLoginLayout.createSequentialGroup()
-                                .addComponent(LabelUsuario)
-                                .addGap(7, 7, 7)
-                                .addComponent(LabelIniciarIMG, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(TxtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(LabelClave, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(TXtClave, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(PanelLoginLayout.createSequentialGroup()
-                        .addGap(85, 85, 85)
-                        .addComponent(BtnIniciarLog, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(PanelLoginLayout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addComponent(jLabel1)))
-                .addContainerGap(21, Short.MAX_VALUE))
-        );
-        PanelLoginLayout.setVerticalGroup(
-            PanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(PanelLoginLayout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addComponent(LabelTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(PanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(PanelLoginLayout.createSequentialGroup()
-                        .addGap(4, 4, 4)
-                        .addComponent(LabelIniciarIMG, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(15, 15, 15))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PanelLoginLayout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(LabelUsuario)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(TxtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(LabelClave)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(TXtClave, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(BtnIniciarLog, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addContainerGap())
-        );
-
-        getContentPane().add(PanelLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 10, 280, 400));
-
-        PanelPrincipal.setBackground(new java.awt.Color(153, 153, 153));
-
-        javax.swing.GroupLayout PanelPrincipalLayout = new javax.swing.GroupLayout(PanelPrincipal);
-        PanelPrincipal.setLayout(PanelPrincipalLayout);
-        PanelPrincipalLayout.setHorizontalGroup(
-            PanelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 430, Short.MAX_VALUE)
-        );
-        PanelPrincipalLayout.setVerticalGroup(
-            PanelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 420, Short.MAX_VALUE)
-        );
-
-        getContentPane().add(PanelPrincipal, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 430, 420));
-
-        LabelFondoIMG.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/login.jpg"))); // NOI18N
-        getContentPane().add(LabelFondoIMG, new org.netbeans.lib.awtextra.AbsoluteConstraints(431, 0, 260, 420));
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/LoginMedicina.jpg"))); // NOI18N
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 730, 520));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnIniciarLogActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnIniciarLogActionPerformed
+        // TODO add your handling code here:
+
+        ValidarInicio();
+
+    }//GEN-LAST:event_BtnIniciarLogActionPerformed
 
     private void TxtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtUsuarioActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_TxtUsuarioActionPerformed
 
-    private void BtnIniciarLogActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnIniciarLogActionPerformed
+    private void BtnIniciarLogKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_BtnIniciarLogKeyPressed
         // TODO add your handling code here:
-        
-        ValidarInicio();
-        
-    }//GEN-LAST:event_BtnIniciarLogActionPerformed
+    }//GEN-LAST:event_BtnIniciarLogKeyPressed
+
+    private void TXtClaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TXtClaveActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TXtClaveActionPerformed
+
+    private void TXtClaveKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_TXtClaveKeyPressed
+       if(evt.getKeyCode()== KeyEvent.VK_ENTER){
+       
+       ValidarInicio();
+       
+       }
+    }//GEN-LAST:event_TXtClaveKeyPressed
+
+    private void TxtUsuarioKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_TxtUsuarioKeyPressed
+    
+        if(evt.getKeyCode()== KeyEvent.VK_ENTER){
+       
+       ValidarInicio();
+       
+       }
+
+    }//GEN-LAST:event_TxtUsuarioKeyPressed
+
+    private void BotonSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonSalirActionPerformed
+        dispose();
+    }//GEN-LAST:event_BotonSalirActionPerformed
+
+    private void BotonSalirKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_BotonSalirKeyPressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonSalirKeyPressed
 
     /**
      * @param args the command line arguments
@@ -232,16 +226,13 @@ public class LoginG extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotonSalir;
     private javax.swing.JButton BtnIniciarLog;
+    private javax.swing.JLabel Fondo;
+    private javax.swing.JLabel IconoLogin;
     private javax.swing.JLabel LabelClave;
-    private javax.swing.JLabel LabelFondoIMG;
-    private javax.swing.JLabel LabelIniciarIMG;
-    private javax.swing.JLabel LabelTitulo;
     private javax.swing.JLabel LabelUsuario;
-    private javax.swing.JPanel PanelLogin;
-    private javax.swing.JPanel PanelPrincipal;
     private javax.swing.JPasswordField TXtClave;
     private javax.swing.JTextField TxtUsuario;
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
